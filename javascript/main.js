08988c57ab46fd464d267e54898c0348bcbbe80d
Bach <bachdangtuan93@gmail.com>
2021-09-28 00:17:11 +0700

    update backtotop

--- javascript/main.js
+++ javascript/main.js
@@ -40,3 +40,20 @@ lightGallery(document.getElementById("pic1_2-thumbnails"), {
 //   exThumbImage: "data-exthumbimage"
 // });
 
+// BACK TO TOP
+$(document).ready(function(){
+	$(window).scroll(function () {
+			if ($(this).scrollTop() > 50) {
+				$('#back-to-top').fadeIn();
+			} else {
+				$('#back-to-top').fadeOut();
+			}
+		});
+		// scroll body to 0px on click
+		$('#back-to-top').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		});
+});
